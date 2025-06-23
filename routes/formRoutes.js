c45e1a4814fa3a36f4a6b6240dbeb549a1e884dd
tuanbe27 <58541533+tuanbe27@users.noreply.github.com>
2025-06-23 10:55:02 +0000

    add multer to read form data

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const formController = require('../controllers/formController');
 const healthController = require('../controllers/healthController');
+const multer = require('multer'); // Import multer
+const upload = multer(); // Khởi tạo multer (không lưu file vào đĩa)
 const router = express.Router();
 
 // Định nghĩa route POST cho việc gửi form
 // Route này sẽ là /api/form/submit (do cách mount router trong server.js)
-router.post('/submit', formController.submitForm);
+router.post('/submit', upload.none(), formController.submitForm);
 
 router.get('/healthcheck', healthController.checkHealth); // API healthcheck
 
